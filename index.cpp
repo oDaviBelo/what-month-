85d8985bfc4cfb6c8ed92195039513832bf6c374
Davi Belo <davibelocontato@gmail.com>
2022-06-27 16:41:31 -0300

    First Commit

---
+++ index.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int M;
+
+    cin >> M;
+
+    switch (M) {
+        case 1:
+        cout<< "Janeiro"<<endl;
+        break;
+
+        case 2:
+        cout<< "Fevereiro"<<endl;
+        break;
+
+        case 3:
+        cout<< "Março"<<endl;
+        break;
+
+        case 4:
+        cout<< "Abril"<<endl;
+        break;
+
+        case 5:
+        cout<< "Maio"<<endl;
+        break;
+
+        case 6:
+        cout<< "Junho"<<endl;
+        break;
+
+        case 7:
+        cout<< "Julho"<<endl;
+        break;
+
+        case 8:
+        cout<< "Agosto"<<endl;
+        break;
+
+        case 9:
+        cout<< "Setembro"<<endl;
+        break;
+
+        case 10:
+        cout<< "Outubro"<<endl;
+        break;
+
+        case 11:
+        cout<< "Novembro"<<endl;
+        break;
+
+        case 12:
+        cout<< "Dezembro"<<endl;
+        break;
+    }
+
+    return 0;
+}
